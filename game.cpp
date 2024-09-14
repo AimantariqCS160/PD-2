63206309266b1f4d56ee30e2efde79c8a6ddb68a
AimantariqCS160 <aimantariq577@gmail.com>
2024-09-14 23:13:45 +0500

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+   system("color 87");
+cout<<"    ------------------------------"<<endl;
+cout<<"           o    ^__^ "<<endl;
+cout<<"            o   (oo) \\___________"<<endl;
+cout<<"                (__) \\           )\\/\\"<<endl;
+cout<<"                     ||  -------w |"<<endl;
+cout<<"                     ||          ||";
+}
